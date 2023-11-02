ca2acb3965934ed327a068702964515b7ae0f3af
ajjankowski <aj.jankowski92@gmail.com>
2023-11-02 08:30:14 +0100

    ts-fundamentals added

---
+++ tests/2-ts-fundamentals.ts
@@ -0,0 +1,10 @@
+let firstName: string = 'John';
+let lastName: string = 'Smith';
+let age: number = 25;
+
+type Customer = { firstName: string, lastName: string, active: boolean };
+let firstCustomer: Customer = {
+    firstName: 'Mary',
+    lastName: 'Johns',
+    active: true
+};
