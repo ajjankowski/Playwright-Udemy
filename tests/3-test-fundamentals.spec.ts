ea3c9b04f96d7b54434a0c51582da65b91796aa4
ajjankowski <aj.jankowski92@gmail.com>
2024-04-24 14:05:58 +0200

    3-installation-update

--- tests/3-test-fundamentals.spec.ts
+++ tests/3-test-fundamentals.spec.ts
@@ -1,3 +1,9 @@
+//Install Playwright: install node.js, install VS Code, terminal (Ctrl+`): npm init playwright@latest
+//Choose in terminal: TS, tests, false, true
+//npm install -> instaluje pakiety, któe są w package.json
+//*.spec.ts -> file with tests
+//npm (Node Pangace Mangaer)
+//npx (Node Package Execute)
 //Run tests from CLI: npx playwright test
 //Run tests in UI mode: npx playwright test --ui
 //Run tests from browser: npx playwright test --project=chromiom
@@ -9,7 +15,7 @@
 //Trace results in report: npx playwright test --trace on
 //Run tests on debug mode: npx playwright test --debug
 
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'; 
 
 //Minimal test structure
 test('first test', async ({page}) => {
@@ -27,7 +33,7 @@ test.describe('test suite 1', () => {
 });
 
 //Test hooks
-//Hooks inside test.describe(.. will be available only for the suit
+//Hooks inside test.describe(.. will be available only for the suit)
 
 //beforeAll
 console.log('Before All Hook');
@@ -44,21 +50,21 @@ console.log('After Each Hook');
 
 test('has title', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
+  //Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test.skip('has title, skipped', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
+  //Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
 
-  // Click the get started link.
+  //Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
+  //Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
